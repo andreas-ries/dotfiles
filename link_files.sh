aaf6621b36a375d2b84d6365d821a909eb82fca2
andreas-ries <and.ries@icloud.com>
2023-04-17 08:42:31 +0200

    Add ssh key liönk

--- link_files.sh
+++ link_files.sh
@@ -9,3 +9,7 @@ ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 echo 'Setting link for tmux conf'
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
+echo 'Setting link for ssh keys'
+ln -s ~/dotfiles/.ssh/id_ed25519 ~/.ssh/id_ed25519
+ln -s ~/dotfiles/.ssh/id_ed25519.pub ~/.ssh/id_ed25519i.pub
+
