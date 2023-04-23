1b0529334729961a22b8ab1f2b7f219c25f0678e
andreas-ries <and.ries@icloud.com>
2023-04-23 07:50:09 +0200

    Force link to dotfiles and remove ssh link as done in ansible playbook

--- link_files.sh
+++ link_files.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
 echo 'Setting link for zshrc'
-ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -f -s ~/dotfiles/.zshrc ~/.zshrc
 
 echo 'Setting link for gitconfig'
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -f -s ~/dotfiles/.gitconfig ~/.gitconfig
 
 echo 'Setting link for tmux conf'
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-
-echo 'Setting link for ssh keys'
-ln -s ~/dotfiles/.ssh/id_ed25519 ~/.ssh/id_ed25519
-ln -s ~/dotfiles/.ssh/id_ed25519.pub ~/.ssh/id_ed25519i.pub
+ln -f -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
