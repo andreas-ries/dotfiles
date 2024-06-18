c139e5a00068492bbf3196ec07b9605ba5a616af
andreas-ries <and.ries@icloud.com>
2024-06-18 21:19:52 +0200

    update

--- installer/get-lazygit.sh
+++ installer/get-lazygit.sh
@@ -8,7 +8,7 @@ if [ "$(uname)" == "Darwin" ]; then
 	PACKAGE_OS=Darwin_x86_64
 fi
 
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_${PACKAGE_OS}.tar.gz"
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_${PACKAGE_OS}.tar.gz"
 
 tar xf lazygit.tar.gz lazygit
 
